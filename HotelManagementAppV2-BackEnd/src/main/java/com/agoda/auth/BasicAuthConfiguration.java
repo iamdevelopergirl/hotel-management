3928ebe524f7f498cd5bd7d92c1ec7d7903be0b3
Manoj Paramasivam <manoj.p.486@gmail.com>
2020-03-09 23:57:07 +0800

    Update BasicAuthConfiguration.java

--- HotelManagementAppV2-BackEnd/src/main/java/com/agoda/auth/BasicAuthConfiguration.java
+++ HotelManagementAppV2-BackEnd/src/main/java/com/agoda/auth/BasicAuthConfiguration.java
@@ -1,24 +1,41 @@
 package com.agoda.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class BasicAuthConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new CustomLogoutSuccessHandler();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/api/**").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                //.formLogin().and()
-                .httpBasic();
+                .and().httpBasic();
+
+        http.httpBasic().and().logout()
+                .clearAuthentication(true)
+                .logoutUrl("/logout")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .logoutSuccessHandler(logoutSuccessHandler())
+                .permitAll()
+                .and().sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+                .expiredUrl("/");
     }
 }
