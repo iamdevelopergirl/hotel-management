ec5a7e317fb2d4e603617f7517d2127533580acb
Elakya <v_elakya@symantec.com>
2020-03-11 02:47:42 +0530

    Changing the folder structure

---
+++ webapp/build/precache-manifest.4516311622ecff89016b9d25a79df29a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881a012eabae37fb30f8fba43729612d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1469476846fee65e46a6",
+    "url": "/static/css/main.dd126736.chunk.css"
+  },
+  {
+    "revision": "32d67db531835441b66b",
+    "url": "/static/js/2.49d561d7.chunk.js"
+  },
+  {
+    "revision": "ff2a223b51b86718de583271daa0b126",
+    "url": "/static/js/2.49d561d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1469476846fee65e46a6",
+    "url": "/static/js/main.9aed4c44.chunk.js"
+  },
+  {
+    "revision": "9fae5fadd334a5a80f4c",
+    "url": "/static/js/runtime-main.1436f226.js"
+  },
+  {
+    "revision": "748a84cefc161e129096443fe21466ff",
+    "url": "/static/media/banner.748a84ce.jpg"
+  },
+  {
+    "revision": "e2ac36a60b5ddecf22884502463b0816",
+    "url": "/static/media/bg-anchor-up.e2ac36a6.svg"
+  },
+  {
+    "revision": "9de67e204c1a8d82ff32b784e19156a1",
+    "url": "/static/media/bg-img.9de67e20.jpg"
+  },
+  {
+    "revision": "7a96d767e00ead2a6c28bcc9e8fa3257",
+    "url": "/static/media/btn-grid-down.7a96d767.svg"
+  },
+  {
+    "revision": "39f8ae0bfe898f5f2e711d500f0aee19",
+    "url": "/static/media/btn-grid-inactive.39f8ae0b.svg"
+  },
+  {
+    "revision": "91bef29d853dbb691f2d8d4a2bffe2b5",
+    "url": "/static/media/btn-grid-up.91bef29d.svg"
+  },
+  {
+    "revision": "05a3e2a0640bca073df09af4ecb7ea03",
+    "url": "/static/media/btn-list-down.05a3e2a0.svg"
+  },
+  {
+    "revision": "ff7151e51d411c538879226fdb5a7be2",
+    "url": "/static/media/btn-list-inactive.ff7151e5.svg"
+  },
+  {
+    "revision": "d1fdb5c076521bf82c448dc85a207881",
+    "url": "/static/media/btn-list-up.d1fdb5c0.svg"
+  },
+  {
+    "revision": "70f3456de9184773bc80575101b8375f",
+    "url": "/static/media/close-logo.70f3456d.png"
+  },
+  {
+    "revision": "5982968eeca9f710346efe40907fd4a7",
+    "url": "/static/media/ic-hotels-grid-settings.5982968e.svg"
+  },
+  {
+    "revision": "fad302929b2c5210c992fcc647244481",
+    "url": "/static/media/ic-mainui-search.fad30292.svg"
+  },
+  {
+    "revision": "fc5d3c57bc48d256ddd18f5d6c86e39b",
+    "url": "/static/media/ic-modal-addresses.fc5d3c57.svg"
+  },
+  {
+    "revision": "b8737790f139d9ef66c1f5631ba1b6ae",
+    "url": "/static/media/ic-nav-add-item.b8737790.svg"
+  },
+  {
+    "revision": "dc285ca73d7b325255b19901e19ad073",
+    "url": "/static/media/ic-tab-ui-setting.dc285ca7.svg"
+  }
+]);
